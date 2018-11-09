419a2caa1ec1a55dd2c01cbc7179051d0873b20e
bankmodefi <bankmodefi@gmail.com>
2018-11-09 14:23:56 +0700

    first

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>Law &mdash; Free Multi-Purpose One Page Bootstrap 4 Theme by uicookies.com</title>
+		<meta name="description" content="Free Bootstrap 4 Theme by uicookies.com">
+		<meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+    
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600|Montserrat:200,300,400" rel="stylesheet">
+
+		<link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/law-icons/font/flaticon.css">
+
+    <link rel="stylesheet" href="assets/fonts/fontawesome/css/font-awesome.min.css">
+    
+    
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/slick-theme.css">
+
+    <link rel="stylesheet" href="assets/css/helpers.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body data-spy="scroll" data-target="#pb-navbar" data-offset="200">
+    
+    <nav class="navbar navbar-expand-lg navbar-dark pb_navbar pb_scrolled-light" id="pb-navbar">
+      <div class="container">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#probootstrap-navbar" aria-controls="probootstrap-navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span><i class="ion-navicon"></i></span>
+        </button>
+        
+      </div>
+    </nav>
+    <!-- END nav -->
+
+    <section class="pb_cover_v1 text-left cover-bg-black cover-bg-opacity-4" style="background-image: url(assets/images/1900x1200_img_7.jpg)" id="section-home">
+      <div class="container">
+        <div class="row align-items-center justify-content-end">
+          <div class="col-md-6  order-md-1">
+              
+
+            <h2 class="heading mb-3">Siwakorn Wongpaiboon</h2>
+            
+            
+            
+            
+          </div>  
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+    
+    
+    <section class="pb_section pb_section_v1" data-section="about" id="section-about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 pr-md-5 pr-sm-0">
+            <h2 class="mt-0 heading-border-top mb-3 font-weight-normal">Profile</h2>
+            <p>Name: Siwakorn</p>
+            <p>Lastname: Wongpaiboon</p>
+            <p>Nickname: Bank</p>
+            <p>Burapha University</p>
+            <p>Informatics computer sci</p>
+            <p>Email: bankmodefi@gmail.com</p>
+            <p>Tel: 0882203602 </p>
+          </div>
+          <div class="col-lg-7">
+            <div class="images">
+              <img class="img1 img-fluid" src="assets/images/a1.jpg" >
+              <img class="img2" src="assets/images/a2.jpg">
+            </div>
+          </div>
+          
+        </div>
+      </div>  
+    </section>
+    <!-- END section -->
+
+    
+    
+	</body>
+</html>
